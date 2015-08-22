a69991cc52f8e6216eb4ab1273bc7a3b465f5571
Byron Heads <byron.heads@gmail.com>
2015-08-22 10:39:01 -0400

    Cleaning up swimming, added water layer

--- scn/player/Player.gd
+++ scn/player/Player.gd
@@ -2,6 +2,8 @@
 extends RigidBody2D
 
 var inTheAir = -1
+var maxAccel = 100
+var waterFriction = 10
 
 func _ready():
 	set_process(true)
@@ -10,12 +12,14 @@ func _ready():
 func _process(delta):
 	# simple floating
 	if(inTheAir != 0 && get_global_pos().y > 0):
-		set_gravity_scale(0.1)
-		set_friction(4)
+		set_gravity_scale(0)
+		set_friction(10)
 		inTheAir = 0
+		var v = get_linear_velocity()
+		set_linear_velocity(Vector2(v.x, v.y / 2)) # hitting the water slows you down
 		print("swimming mode")
 	elif(inTheAir != 1 && get_global_pos().y <= 0):
-		set_gravity_scale(3)
+		set_gravity_scale(10)
 		set_friction(1)
 		inTheAir = 1
 		print("flying mode")
@@ -28,9 +32,31 @@ func _process(delta):
 	var dX = g.x - p.x
 	set_rot(deg2rad(atan2(dY, dX) * 180 / PI))
 
+	
+
 	# acceleration
+	var v = get_linear_velocity()
 	if(inTheAir == 0 && Input.get_mouse_button_mask() & 1):
-		var v = get_linear_velocity()
-		set_linear_velocity(Vector2(v.x + dX * delta, v.y +  -dY * delta))
+		set_linear_velocity(Vector2(v.x + clamp(dX * delta, -maxAccel, maxAccel), v.y + clamp(-dY * delta, -maxAccel, maxAccel)))
+	elif(inTheAir == 0): # water friction
+		var nv = Vector2()
+		var xFrict = max(abs(v.x / 3), 10)
+		var yFrict = max(abs(v.y / 2), 10)
+		if(1 < v.x):
+			nv.x = v.x - (xFrict * delta)
+		if(v.x < -1):
+			nv.x = v.x + (xFrict * delta)
+		if(-1 <= v.x && v.x <= 1):
+			nv.x = 0
+		
+		if(1 < v.y):
+			nv.y = v.y - (yFrict * delta)
+		if(v.y < -1):
+			nv.y = v.y + (yFrict * delta)
+		if(-1 < v.y && v.y <= 1):
+			nv.y = 0
+		set_linear_velocity(nv) 
+	
+	print(get_linear_velocity())
 
 
